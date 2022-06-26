95d3800f5e6e7bc751b9abf8a36d40f4e7303c97
Danteie <aleksandar.djuratovic93@gmail.com>
2022-06-26 10:41:09 +0200

    small styling

--- src/Component/Text/Text.js
+++ src/Component/Text/Text.js
@@ -7,7 +7,6 @@ function Text({text,title}) {
             <div className="article-text">
                 <h1 className="article-title">{title}</h1> 
                 <p className="article-p">{text}</p>
-                <a href="test">SAZNAJ VISE</a>
             </div>
         </article>
     );
