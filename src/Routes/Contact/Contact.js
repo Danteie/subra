615747c55675f16cbb0c7f01a7f3da95fd78c159
Danteie <aleksandar.djuratovic93@gmail.com>
2022-06-29 12:25:09 +0200

    adding kontakt

--- src/Routes/Contact/Contact.js
+++ src/Routes/Contact/Contact.js
@@ -1,17 +1,30 @@
 import React from "react";
+import './Contact.css'
+
 //Home
 import Logo from '../../Component/Logo/Logo'
 import Menu from '../../Component/Menu/Menu'
+//Body
+
 
 
 function Contact() {
     return(
-        <div>
+        <div className="contact-body">
           <div className='header'>
             <Logo/>
             <Menu/>
         </div>
-  
+        <div className="contact-countainer">
+            <article>
+                <div className="contact-text">
+                    <h1>Kontakt</h1>
+                    <p>Br telefona: +382 69 430 070</p>
+                    <p>email: test@test.com</p>
+                    <p>adresa: <a href="www.google.com">Subra</a></p>
+                </div>
+             </article>   
+        </div>
         </div>
     );
 }
